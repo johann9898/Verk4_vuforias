c7e628803f16b332f11f91882a17d3abdf8fd3f0
Jóhann Rúnarsson <johann9898@gmail.com>
2017-11-12 21:18:23 +0000

    Vuforia
    verk4 vef

---
+++ Assets/Script/ModelController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class ModelController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private Animator anim;
+    private Vector3 velocity;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = CrossPlatformInputManager.GetAxis("Horizontal");
+        float y = CrossPlatformInputManager.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(x, 0, y);
+
+        rb.velocity = movement * 2f;
+
+        if (x != 0 && y != 0)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(x,y) * Mathf.Rad2Deg,transform.eulerAngles.z);
+        }
+
+        if (x != 0 || y != 0)
+        {
+            anim.SetFloat("Speed", 1);
+            //anim.SetFloat("Direction", x);
+        }
+        else
+        {
+            anim.SetFloat("Speed", 0);
+        }
+
+        transform.localPosition += rb.velocity * Time.fixedDeltaTime;
+
+    }
+}
